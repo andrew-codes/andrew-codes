4a7d70744e186edfe68cdf584b73e5a73c9b314b
Andrew Smith <andrew@andrew.codes>
2023-06-16 13:29:11 +0000

    Error pages and resolved bug with refreshing cache
    Fixes #63

--- app/root.tsx
+++ app/root.tsx
@@ -1,97 +1,55 @@
-import {
-  Links,
-  LiveReload,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react"
+import { Outlet } from "@remix-run/react"
 import type { FC } from "react"
-import { MetronomeLinks } from "@metronome-sh/react"
 import styled from "styled-components"
-import GlobalStyles from "./components/GlobalStyles"
-import GlobalNav from "./components/MainNav"
-import { useNonce } from "./libs/NonceProvider"
-import favicon from "./images/favicon-48.png"
-import favicon1024 from "./images/favicon-1024.png"
+import ApplicationShell from "./components/ApplicationShell"
+import { Header } from "./components/Category"
+import Link from "./components/Link"
+import PageWithHeader from "./components/PageWithHeader"
+import { Blockquote, Paragraph } from "./components/Post"
 
-const Root = styled.div`
-  max-width: 1200px;
-  min-width: 390px;
-  margin: 0 auto;
+const App: FC<{}> = () => (
+  <ApplicationShell>
+    <Outlet />
+  </ApplicationShell>
+)
 
-  > * {
-    margin: 0 2rem;
-    text-size-adjust: 100%;
+const Page = styled(PageWithHeader)`
+  section {
+    padding: 0 1.5rem 1.5rem;
 
     @media (max-width: 640px) {
       margin: 0 0.5rem;
     }
+
+    > ${Paragraph} {
+      margin-bottom: 1.125rem;
+    }
   }
 `
 
-const App: FC<{}> = () => {
-  const nonce = useNonce()
-
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link
-          crossOrigin="anonymous"
-          rel="preload"
-          as="font"
-          href={`/fonts/Lato-Regular.ttf`}
-        />
-        <link
-          crossOrigin="anonymous"
-          rel="preload"
-          as="font"
-          href={`/fonts/Lato-Bold.ttf`}
-        />
-        <link
-          crossOrigin="anonymous"
-          rel="preload"
-          as="font"
-          href={`/fonts/Lato-Thin.ttf`}
-        />
-        <link
-          crossOrigin="anonymous"
-          rel="preload"
-          as="font"
-          href={`/fonts/Lato-Black.ttf`}
-        />
-        <link
-          crossOrigin="anonymous"
-          rel="preload"
-          as="font"
-          href={`/fonts/Lato-Light.ttf`}
-        />
-        <meta name="og:title" content="Senior Software Engineer" />
-        <meta name="og:type" content="website" />
-        <link rel="shortcut icon" href={favicon} />
-        <link rel="apple-touch-icon" href={favicon1024} />
-        <link
-          crossOrigin="anonymous"
-          rel="stylesheet"
-          type="text/css"
-          href={`/css/dracula.css`}
-        />
-      </head>
-      <body>
-        <MetronomeLinks nonce={nonce} />
-        <Links />
-        <GlobalStyles />
-        <GlobalNav />
-        <Root>
-          <Outlet />
-        </Root>
-        <ScrollRestoration nonce={nonce} />
-        <Scripts nonce={nonce} />
-        <LiveReload nonce={nonce} />
-      </body>
-    </html>
-  )
-}
+const ErrorBoundary: FC<{}> = () => (
+  <ApplicationShell>
+    <Page>
+      <Header category={null}>
+        <h1>Oops...</h1>
+      </Header>
+      <section>
+        <Paragraph>
+          Well that's embarrassing! There's been an error, but don't worry, you
+          can use the main navigation to head back to the homepage.
+        </Paragraph>
+        <Blockquote>
+          If you feel you're seeing this message in error, please feel free to
+          contact me or open a{" "}
+          <Link to="https://github.com/andrew-codes/andrew-codes/issues/new">
+            GitHub issue
+          </Link>
+          . No pressure though!
+        </Blockquote>
+      </section>
+    </Page>
+  </ApplicationShell>
+)
 
 export default App
+export { ErrorBoundary }
