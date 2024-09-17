0f86278e7ae4b3b833fcd3771363a970aec44a9c
Andrew Smith <andrew@andrew.codes>
2024-09-17 11:37:47 -0400

    update devtools

--- app/posts/2023/devtools/assets/ls-vscode-extensions.code.sh
+++ app/posts/2023/devtools/assets/ls-vscode-extensions.code.sh
@@ -1,10 +1,16 @@
-#! /bin/bash
+echo "Ensure the latest bash is installed if using OSX (brew install bash)"
 
-markdown_list_output="|Extension Name | Description |
+markdown_list_output="| Extension Name | Description |
 |:--|:--|"
 markdown_install_output="\`\`\`bash"
-readarray -t extensions <<<$(code --list-extensions)
+readarray -t extensions < <(code --list-extensions)
+echo "Total extensions: ${#extensions[@]}"
 for val in "${extensions[@]}"; do
+
+    markdown_install_output="$markdown_install_output
+code --install-extension $val"
+
+    echo "Looking up information on $val"
     extension_data=$(
         curl \
             -s \
@@ -24,13 +30,13 @@ $(
             jq '"|[\(.results[0].extensions[0].displayName)](https://marketplace.visualstudio.com/items?itemName=\(.results[0].extensions[0].publisher.publisherName).\(.results[0].extensions[0].extensionName)) | \(.results[0].extensions[0].shortDescription) |"' |
             sed 's/\"//g'
     )"
-
-    markdown_install_output="$markdown_install_output
-code --install-extension $val"
 done
+
 markdown_install_output="$markdown_install_output
 \`\`\`"
 
-echo "$markdown_list_output
+output="$markdown_list_output
 
 $markdown_install_output"
+
+echo "$output"
