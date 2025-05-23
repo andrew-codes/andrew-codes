43b056046380db1f15853360d2aede652d22f7bc
Andrew Smith <andrew@andrew.codes>
2025-05-23 15:40:45 -0400

    feat: add remaining recommendations

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,15 +13,12 @@ import { fileURLToPath } from "url"
 import CallToAction from "../components/CallToAction"
 import PageHeader from "../components/PageHeader"
 import PostCard from "../components/PostCard"
-import Recommendation from "../components/Recommendation"
 import { Section, SectionHeader } from "../components/Section"
 import { getFilePartsToHash, getHash } from "../libs/hash.server"
 import { getMdxPages } from "../libs/mdx.server"
 import { getServerTimeHeader } from "../libs/timing.server"
 import { useLoaderHeaders } from "../libs/utils"
-import darnell from "../public/images/darnell.jpeg"
-import denise from "../public/images/denise.jpeg"
-import rick from "../public/images/rick-cabrera.jpeg"
+import { featured } from "../recommendations"
 
 const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const timings = {}
@@ -106,7 +103,7 @@ const HomeRoute = () => {
         </Typography>
         <CallToAction
           secondaryTitle="View Recommendations"
-          secondaryAction="/recommendations"
+          secondaryAction="/recommendations?priority=featured"
         />
       </PageHeader>
       <Section>
@@ -124,98 +121,9 @@ const HomeRoute = () => {
           </Button>
         </SectionHeader>
         <Stack direction="column" spacing={2}>
-          <Recommendation
-            summarized
-            profileImage={denise}
-            name="Denise Architetto"
-            company="Microsoft"
-            title="
-Principal Group Engineering Manager (Director)"
-          >
-            <Typography level="body-md" sx={{ marginBottom: 2 }}>
-              Andrew is a highly talented and passionate engineer who
-              consistently brings innovation, precision, and thoughtfulness to
-              everything he does. He's a true trailblazer—always exploring
-              modern technologies to solve complex legacy challenges and driving
-              engineering productivity and efficiency across the board.
-            </Typography>
-            <Typography level="body-md" sx={{ marginBottom: 2 }}>
-              Within the team, Andrew is often referred to as “the professor”—a
-              well-earned nickname that speaks to his natural ability to mentor
-              and guide others. He excels at helping early-in-career engineers
-              grow, teaching them solid engineering practices, design patterns,
-              and how to effectively break down complex problems into clear,
-              manageable solutions.
-            </Typography>
-            <Typography level="body-md" sx={{ marginBottom: 2 }}>
-              Andrew takes great pride in his work and has been instrumental in
-              designing and implementing core engineering solutions for
-              Microsoft's Content Management System (CMS). His contributions
-              have been critical to ensuring the reliability and scalability of
-              a platform that handles over 15 billion requests per month.{" "}
-            </Typography>
-            <Typography level="body-md" sx={{ marginBottom: 2 }}>
-              He combines deep technical expertise with a passion for quality,
-              mentorship, and continuous improvement. I highly recommend Andrew
-              to any organization looking for a principled and forward-thinking
-              engineer who leads with both skill and generosity.
-            </Typography>
-          </Recommendation>
-          <Recommendation
-            summarized
-            profileImage={rick}
-            name="Rick Cabrera"
-            company="Experience LLC."
-            title="Chief Technology Officer CTO"
-          >
-            <Typography level="body-md" sx={{ marginBottom: 2 }}>
-              Andrew is a fantastic software developer. He was hired on at
-              Experience to bring focus and leadership to our front-end
-              technology stack and his impact has been significant. His
-              technical acumen and ability to work extremely well with our
-              design team has brought beautiful and functional components, along
-              with consistency and improved development speed to our team.
-            </Typography>
-            <Typography level="body-md" sx={{ marginBottom: 2 }}>
-              His commitment to personal growth and to his team is beyond
-              reproach. Aside from constantly learning new technologies and
-              building hobby projects, Andrew is a thoughtful developer and
-              leader that seeks and is very responsive to feedback from his
-              peers and managers. I'm excited for what the future holds for
-              Andrew and recommend him highly. While he'd be a significant asset
-              to any team looking for immediate technical implementation and
-              leadership, his potential is immense and very much worth
-              consideration for your team.
-            </Typography>
-          </Recommendation>
-
-          <Recommendation
-            summarized
-            profileImage={darnell}
-            name="Darnell Brown"
-            company="Microsoft"
-            title="Principal Software Engineer"
-          >
-            <Typography level="body-md" sx={{ marginBottom: 2 }}>
-              Where do I start. Andrew's impact has been felt at all levels of
-              our team and he is as gifted and well seasoned of any engineer
-              that I have ever seen and witnessed firsthand. He flourishes in
-              ambiguity. He excels in the unknown. He leads when others are not
-              watching, and he priorities and commands engineering excellence
-              through his intentional actions of continuous improvement with a
-              strong affinity for effectively establishing unique and
-              collaborative mechanisms. His front end ability is unmatched, but
-              more than that, he has continuously elevated the engineers around
-              him to think and breath through a value driven lens of quality.
-              This is what sets Andrew apart. Andrew is the principal,
-              distinguished, staff engineer that could help lead any team to new
-              heights and achievements. Andrew's selfless and humble approach to
-              growth and improvement is one that I respect so much given his
-              tenure in the industry. He's a leader that isn't afraid to take
-              feedback. He simply receives and gives back a continuously
-              improved version of himself. Hire the man!!
-            </Typography>
-          </Recommendation>
+          {featured.map((Recommendation, index) => (
+            <Recommendation key={index} />
+          ))}
         </Stack>
       </Section>
       <Divider />
